d98000e5875d46caf4fea7ddd06f682e9123a927
NguyenTiepThang <nguyenthangDzTokyo@gmail.com>
2021-01-02 19:49:06 +0900

    portfolio

---
+++ js/portfolio.js
@@ -0,0 +1,159 @@
+const openMenu = document.querySelector('.navbar__button');
+const checkMenu = document.querySelector('.navbar-nav-tablet');
+const contactBTN = document.querySelector('.contact-btn');
+const disable =document.querySelectorAll('.disable');
+const mobieNav = document.querySelectorAll('.nav__item-tablet');
+
+
+
+        openMenu.onclick = () =>{
+            openMenu.classList.toggle('open');
+            checkMenu.classList.toggle('open__Menu');
+        
+        }
+        contactBTN.onclick = () =>{
+            document.querySelectorAll('.contact__item').forEach(item =>{
+                item.classList.toggle('fade')
+        })
+        }
+       mobieNav.forEach(item =>{
+           item.onclick = () =>{
+            openMenu.classList.remove('open');
+            checkMenu.classList.remove('open__Menu');
+
+               
+           }
+       })
+
+
+        // slide 
+        // var counter = 1;
+        // setInterval(function(){
+        // document.getElementById('radio' + counter).checked = true;
+        // counter++;
+        // if(counter > 4){
+        //     counter = 1;
+        // }
+        // }, 10000);
+
+
+
+//skills 
+
+    function popup(data) {
+
+        const skill = document.querySelector(data.skill);
+        const skillItem = skill.querySelectorAll('.skill__item .skill__icon ');
+        const type = data.type;
+
+        skillItem.forEach(item =>{
+            item.onmouseover = (e)=>{
+                e.stopPropagation();
+               
+                var name = e.currentTarget.getAttribute('icon-name');
+                var content = e.currentTarget.querySelector('.item__content .item__title');
+                var contentNumber = e.currentTarget.querySelector('.item__content .item__number');
+                var contentDEV = e.currentTarget.querySelector('.item__content');
+                var pacentColor = contentDEV.querySelector('.pacent__color');
+
+
+               
+              
+
+                var pacent = 0;
+                type.forEach(item =>{
+                    if(item.name === name){
+                        pacent = item.value
+                    }
+                })
+            // requestAnimationFrame    
+                if(name !== null){
+                    var start = 0;
+                    const count = ()=>{
+                        start++;
+                        contentNumber.textContent = `${start} %`;
+                        if(start > 85){
+                            contentNumber.style.color = `red`;
+                        }else{
+                            contentNumber.style.color = `black`;
+                        }
+                        if(start < pacent){
+                            requestAnimationFrame(count)
+                        }
+                    }
+                    requestAnimationFrame(count)
+             // requestAnimationFrame  
+            
+                    contentDEV.style.opacity = `${1}`;
+                    contentDEV.style.transform = `translateY(${0})`;
+                    pacentColor.style.transform = `translateX(${pacent-100}%)`
+
+                    content.innerText = `${name} :`
+
+                    console.log(pacent)
+
+                }
+
+            }
+            item.onmouseout =(e)=>{
+                e.stopPropagation();
+                var content = e.currentTarget.querySelector('.item__content .item__title');
+                var contentDEV = e.currentTarget.querySelector('.item__content');
+                var pacentColor = contentDEV.querySelector('.pacent__color');
+
+                contentDEV.style.opacity = `${0}`
+                contentDEV.style.transform = `translateY(${-40}px)`;
+                pacentColor.style.transform = `translateX(${-100}%)`
+                content.innerText = ''
+            }
+        })
+     
+
+}
+popup({
+    skill : '#skill',
+    type : 
+        [
+            {
+                name : 'html',
+                value : 95
+            },
+            {
+                name : 'css',
+                value : 95
+            },
+            {
+                name : 'javascript',
+                value : 80
+            },
+            {
+                name : 'react',
+                value : 70
+            },
+            {
+                name : 'scss',
+                value : 71
+            },
+            {
+                name : 'nodejs',
+                value : 65
+            },
+            {
+                name : 'bootstrap',
+                value : 80
+            },
+            {
+                name : 'mongodb',
+                value : 60
+            }
+        ]
+})
+
+
+// footer 
+disable.forEach(item =>{
+    item.onclick = (e) =>{
+        e.preventDefault();
+    }
+})
+
